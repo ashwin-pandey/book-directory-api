03ea63b6738c2502b57e2fa3e344206bbb3eac2c
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2022-01-22 18:59:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { AuthorRouter } = require('./routes/author.routes');
+const { BooksRouter } = require('./routes/books.routes');
+const app = express();
+
+/**ENV CONFIG */
+require('dotenv').config({path: __dirname + '/.env'});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/**CORS HEADERS */
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT , PATCH, DELETE, OPTIONS"
+  );
+  res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-Requested-With,  x-xsrf-token, Content-Type, Authorization"
+  );
+  next();
+});
+
+/**DB CONNECT */
+require('./config/db.config');
+
+app.get('/', (req, res) => {
+  res.send("Hello!");
+});
+
+app.use('/books', BooksRouter);
+app.use('/author', AuthorRouter);
+
+const port = process.env.PORT;
+app.listen(port, (error) => {
+    if (error) {
+        console.log(`Error listening on port ${port} - ${error}`);
+    } else {
+        console.log(`Listening on port ${port}`);
+    }
+});
