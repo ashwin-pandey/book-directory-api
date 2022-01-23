846066edf0e6e8a83dedae8b13a1da1df68eb145
Ashwin Pandey <ashwin.pandey11197@gmail.com>
2022-01-23 15:34:44 +0530

    CRUD api for books done with filters

--- routes/books.routes.js
+++ routes/books.routes.js
@@ -4,6 +4,11 @@ const booksController = require('../controller/books.controller');
 
 router.get('/', booksController.getAllBooks);
 router.get('/:id', booksController.getSpecificBook);
+router.post('/', booksController.createBook);
+// router.put('/:id', booksController.patchBook);
+router.patch('/:id', booksController.patchBook);
+router.delete('/:id', booksController.deleteBook);
+
 
 const BooksRouter = router;
 module.exports = { BooksRouter };
